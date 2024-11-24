9ceffa96e3e2135da40de594ca26ff9d538dcf3b
Daniel Braga <dbragabarbosa@gmail.com>
2024-11-24 17:01:27 -0300

    feat: add project

---
+++ AppFoundationsOfComputerScience/AppFoundationsOfComputerScience/AppFoundationsOfComputerScienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFoundationsOfComputerScienceApp.swift
+//  AppFoundationsOfComputerScience
+//
+//  Created by Daniel Braga Barbosa on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AppFoundationsOfComputerScienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppFoundationsOfComputerScience/AppFoundationsOfComputerScienceTests/AppFoundationsOfComputerScienceTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppFoundationsOfComputerScienceTests.swift
+//  AppFoundationsOfComputerScienceTests
+//
+//  Created by Daniel Braga Barbosa on 24/11/24.
+//
+
+import Testing
+@testable import AppFoundationsOfComputerScience
+
+struct AppFoundationsOfComputerScienceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
