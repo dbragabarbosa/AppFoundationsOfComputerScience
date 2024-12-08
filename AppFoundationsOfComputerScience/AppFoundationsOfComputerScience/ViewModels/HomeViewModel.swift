84682c66d8f0f11d5c2b7d35e7ffb5dd553d8da2
Daniel Braga <dbragabarbosa@gmail.com>
2024-12-07 22:52:40 -0300

    feat: add HomeViewModel

--- AppFoundationsOfComputerScience/AppFoundationsOfComputerScience/ViewModels/HomeViewModel.swift
+++ AppFoundationsOfComputerScience/AppFoundationsOfComputerScience/ViewModels/HomeViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class HomeViewModel: ObservableObject
+{
+    @Published var title: String = ""
+    @Published var subtitle: String = ""
+    @Published var buttonText: String = ""
+    
+    func welcome()
+    {
+        title = "Bem vindo"
+        subtitle = "Explore, aprenda e interaja com os nossos conteúdos!"
+        buttonText = "Conteúdos disponíveis"
+    }
+}
