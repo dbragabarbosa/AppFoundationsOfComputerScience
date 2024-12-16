21991421f2603215429b4564919a6451618c835f
Daniel Braga <dbragabarbosa@gmail.com>
2024-12-15 23:05:10 -0300

    feat: Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,8 @@
-// Package.swift
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "AppFoundationsOfComputerScience",
-    platforms: [
-        .iOS(.v13)
-    ],
-    dependencies: [
-        // Adicione dependências aqui, se necessário.
-    ],
-    targets: [
-        .target(
-            name: "AppFoundationsOfComputerScience",
-            dependencies: []),
-        .testTarget(
-            name: "AppFoundationsOfComputerScienceTests",
-    ]
+    products: [],
+    targets: []
 )
